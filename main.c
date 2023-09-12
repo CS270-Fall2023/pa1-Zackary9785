4d96b737334b623696f3ff54c4e19c379a03c095
Zackary <zwmorrow@nic.edu>
2023-09-12 16:54:26 -0700

    stuck on stack smashing error

--- main.c
+++ main.c
@@ -16,26 +16,37 @@
 int main()
 {
     char **arrayOfTokens, str[] = "ls -l file";
-    printf("%s\n", str);
-    //printf("Please enter a string: ");
+    char *newString;
+    //strncat(*arrayOfTokens[1], str, 2);
+    //printf("%s\n", &arrayOfTokens);
+    //printf("%s\n", arrayOfTokens);
+    ////printf("%s\n", str);
+    printf("Please enter a string: ");
+    scanf("%[^\n]", str);
     printf("The length of the string is: %d\n", strlen(str));
     //strncpy((*arrayOfTokens), str, 2);
     int lenOfSubString;
     int j=0;
+    int numberOfTokens = 0;
     for(int i=0; i<strlen(str)+1; i++)
     {
-        j++;
+        if(str[i]!=' ')
+            j++;
+        
         //printf("%s\n", str+i);
-        if(str[i] == ' ' || str[i] == '\0')
+        if((str[i] == ' ' && str[i+1] != ' ') || str[i] == '\0')
         {
-            lenOfSubString = j-1;
+            if(str[i]=='\0')
+                j--;
+            lenOfSubString = j;
             printf("Length of substring is: %i\n", lenOfSubString);
             printf("found space or null\n");
             //copy length lenOfSubString substring of str into array
             j=0;
+            numberOfTokens++;
         }    
     }
-    //int numberOfTokens;
+    printf("The number of tokens is %d\n", numberOfTokens);
     //numberOfTokens = getTokens(str, &arrayOfTokens);
 
     return 0;
